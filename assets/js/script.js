a3aaf3c012b947819532e953aa100cadd1f3c8c0
BaileyMuir <BaileyIRMuir@outlook.com>
2021-07-08 11:10:52 +0000

    add dom selectors to top of page to help with loading time(cache the dom)

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/casch the dom/ 
+let player-score = 0;
+let computer-score = 0;
+let span-player-score = document.getElementById("player-score");
+let span-computer-score = document.getElementById("computer-score");
+let div-score-board = document.querySelector(".score-board");
+let outcome-result-h4 = document.querySelector("#outcome-result > h4")
+let div-rock-selector = document.getElementById("rock")
+let div-paper-selector = document.getElementById("paper")
+let div-scissors-selector = document.getElementById("scissors")
+let div-lizard-selector = document.getElementById("lizard")
+let div-spock-selector = document.getElementById("spock")
