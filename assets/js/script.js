a26b612b59f4d318faac04bc288b2591a517501c
BaileyMuir <BaileyIRMuir@outlook.com>
2021-07-08 12:08:57 +0000

    fixed syntax error with let by replacing - with _ , add number generator , add button activator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,42 @@
 /casch the dom/ 
-let player-score = 0;
-let computer-score = 0;
-let span-player-score = document.getElementById("player-score");
-let span-computer-score = document.getElementById("computer-score");
-let div-score-board = document.querySelector(".score-board");
-let outcome-result-h4 = document.querySelector("#outcome-result > h4")
-let div-rock-selector = document.getElementById("rock")
-let div-paper-selector = document.getElementById("paper")
-let div-scissors-selector = document.getElementById("scissors")
-let div-lizard-selector = document.getElementById("lizard")
-let div-spock-selector = document.getElementById("spock")
+let player_score = 0;
+let computer_score = 0;
+let span_player_score = document.getElementById("player-score");
+let span_computer_score = document.getElementById("computer-score");
+let div_score_board = document.querySelector(".score-board");
+let outcome_result_h4 = document.querySelector("#outcome-result > h4");
+let div_rock_selector = document.getElementById("rock");
+let div_paper_selector = document.getElementById("paper");
+let div_scissors_selector = document.getElementById("scissors");
+let div_lizard_selector = document.getElementById("lizard");
+let div_spock_selector = document.getElementById("spock");
+
+/number generator/ 
+function computer_Selection() {
+    let computer_Options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    let number_Generator = Math.floor(Math.random() * 5);
+    console.log(computer_Options[number_Generator])
+}
+
+computer_Selection()
+
+
+div_rock_selector.addEventListener('click', function() {
+    console.log(rock);
+})
+
+div_paper_selector.addEventListener('click', function() {
+    console.log(paper);
+})
+
+div_scissors_selector.addEventListener('click', function() {
+    console.log(scissors);
+})
+
+div_lizard_selector.addEventListener('click', function() {
+    console.log(lizard);
+})
+
+div_spock_selector.addEventListener('click', function() {
+    console.log(spock);
+})
