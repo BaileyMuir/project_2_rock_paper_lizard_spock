7eeed150a795425da5004bcd50bce62cd3fdf236
BaileyMuir <BaileyIRMuir@outlook.com>
2021-07-08 20:03:46 +0000

    use jshint to find and fix errors by placing semicolons where appropriet

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,66 +24,66 @@ function game(player_Choice) {
     switch(`player_Choice` + `computer_Choice`){
         case 'rockscissors':
         case 'rocklizard':
-            rock_win(player_Choice, computer_Choice)
+            rock_win(player_Choice, computer_Choice);
             break;
         case 'paperrock':
-            paperrock_win(player_Choice, computer_Choice)
+            paperrock_win(player_Choice, computer_Choice);
             break;
         case 'paperspock':
-            paperspock_win(player_Choice, computer_Choice)
+            paperspock_win(player_Choice, computer_Choice);
             break;
         case 'scissorspaper':
-            scissorspaper_win(player_Choice, computer_Choice)
+            scissorspaper_win(player_Choice, computer_Choice);
             break;
         case 'scissorslizard':
-            scissorslizard_win(player_Choice, computer_Choice)
+            scissorslizard_win(player_Choice, computer_Choice);
             break;
         case 'lizardspock':
-            lizardspock_win(player_Choice, computer_Choice)
+            lizardspock_win(player_Choice, computer_Choice);
             break;
         case 'lizardpaper':
-            lizardpaper_win(player_Choice, computer_Choice)
+            lizardpaper_win(player_Choice, computer_Choice);
             break;
         case 'spockrock':
-            spockrock_win(player_Choice, computer_Choice)
+            spockrock_win(player_Choice, computer_Choice);
             break;
         case 'spockscissors':
-            spockscissors_win(player_Choice, computer_Choice)
+            spockscissors_win(player_Choice, computer_Choice);
             break;
         case 'scissorsrock':
         case 'lizardrock':
-            rock_loss(player_Choice, computer_Choice)
+            rock_loss(player_Choice, computer_Choice);
             break;
         case 'rockpaper':
-            rockpaper_loss(player_Choice, computer_Choice)
+            rockpaper_loss(player_Choice, computer_Choice);
             break;
         case 'spockpaper':
-            spockpaper_loss(player_Choice, computer_Choice)
+            spockpaper_loss(player_Choice, computer_Choice);
             break;
         case 'paperscissors':
-            paperscissors_loss(player_Choice, computer_Choice)
+            paperscissors_loss(player_Choice, computer_Choice);
             break;
         case 'lizardscissors':
-            lizardscissors_loss(player_Choice, computer_Choice)
+            lizardscissors_loss(player_Choice, computer_Choice);
             break;
         case 'spocklizard':
-            spocklizard_loss(player_Choice, computer_Choice)
+            spocklizard_loss(player_Choice, computer_Choice);
             break;
         case 'paperlizard':
-            paperlizard_loss(player_Choice, computer_Choice)
+            paperlizard_loss(player_Choice, computer_Choice);
             break;
         case 'rockspock':
-            rockspock_loss(player_Choice, computer_Choice)
+            rockspock_loss(player_Choice, computer_Choice);
             break;
         case 'scissorsspock':
-            scissorsspock_loss(player_Choice, computer_Choice)
+            scissorsspock_loss(player_Choice, computer_Choice);
             break;
         case 'rockrrock':
         case 'sissorssissors':
         case 'paperpaper':
         case 'lizardlizard':
         case 'spockspock':
-            draw(player_Choice, computer_Choice)
+            draw(player_Choice, computer_Choice);
             break;
     }
 }
@@ -91,28 +91,28 @@ function game(player_Choice) {
 function main(){
     div_rock_selector.addEventListener('click', function() {
         game("rock");
-        console.log("rock")
+        console.log("rock");
     })
     
     div_paper_selector.addEventListener('click', function() {
         game("paper");
-        console.log("paper")
+        console.log("paper");
     })
     
     div_scissors_selector.addEventListener('click', function() {
         game("scissors");
-        console.log("scissors")
+        console.log("scissors");
     })
     
     div_lizard_selector.addEventListener('click', function() {
         game("lizard");
-        console.log("lizard")
+        console.log("lizard");
     })
     
     div_spock_selector.addEventListener('click', function() {
         game("spock");
-        console.log("spock")
-    })
+        console.log("spock");
+    });
 }
 
-main()
+main();
