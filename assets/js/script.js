892da7f12b18c75231fba4332355bc8073b71e6a
BaileyMuir <BaileyIRMuir@outlook.com>
2021-07-11 22:01:25 +0000

    organise javascript to improve readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,103 @@ let div_scissors_selector = document.getElementById("scissors");
 let div_lizard_selector = document.getElementById("lizard");
 let div_spock_selector = document.getElementById("spock");
 
+function main(){
+    div_rock_selector.addEventListener('click', function() {
+        game("rock");
+    })
+    
+    div_paper_selector.addEventListener('click', function() {
+        game("paper");
+    })
+    
+    div_scissors_selector.addEventListener('click', function() {
+        game("scissors");
+    })
+    
+    div_lizard_selector.addEventListener('click', function() {
+        game("lizard");
+    })
+    
+    div_spock_selector.addEventListener('click', function() {
+        game("spock");
+    });
+}
+
+main();
+
 function computer_Selection() {
     let computer_Options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     let number_Generator = Math.floor(Math.random() * 5);
     return computer_Options[number_Generator]
+}
+
+function game(player_Choice) {
+    let computer_Choice = computer_Selection();
+    switch(player_Choice + computer_Choice){
+        case 'rockscissors':
+        case 'rocklizard':
+            rock_win(player_Choice, computer_Choice);
+            break;
+        case 'paperrock':
+            paperrock_win(player_Choice, computer_Choice);
+            break;
+        case 'paperspock':
+            paperspock_win(player_Choice, computer_Choice);
+            break;
+        case 'scissorspaper':
+            scissorspaper_win(player_Choice, computer_Choice);
+            break;
+        case 'scissorslizard':
+            scissorslizard_win(player_Choice, computer_Choice);
+            break;
+        case 'lizardspock':
+            lizardspock_win(player_Choice, computer_Choice);
+            break;
+        case 'lizardpaper':
+            lizardpaper_win(player_Choice, computer_Choice);
+            break;
+        case 'spockrock':
+            spockrock_win(player_Choice, computer_Choice);
+            break;
+        case 'spockscissors':
+            spockscissors_win(player_Choice, computer_Choice);
+            break;
+        case 'scissorsrock':
+        case 'lizardrock':
+            rock_loss(player_Choice, computer_Choice);
+            break;
+        case 'rockpaper':
+            rockpaper_loss(player_Choice, computer_Choice);
+            break;
+        case 'spockpaper':
+            spockpaper_loss(player_Choice, computer_Choice);
+            break;
+        case 'paperscissors':
+            paperscissors_loss(player_Choice, computer_Choice);
+            break;
+        case 'lizardscissors':
+            lizardscissors_loss(player_Choice, computer_Choice);
+            break;
+        case 'spocklizard':
+            spocklizard_loss(player_Choice, computer_Choice);
+            break;
+        case 'paperlizard':
+            paperlizard_loss(player_Choice, computer_Choice);
+            break;
+        case 'rockspock':
+            rockspock_loss(player_Choice, computer_Choice);
+            break;
+        case 'scissorsspock':
+            scissorsspock_loss(player_Choice, computer_Choice);
+            break;
+        case 'rockrrock':
+        case 'sissorssissors':
+        case 'paperpaper':
+        case 'lizardlizard':
+        case 'spockspock':
+            draw(player_Choice, computer_Choice);
+            break;
+    }
 }
 
 function rock_win(player_Choice, computer_Choice) {
@@ -145,96 +238,3 @@ function rock_win(player_Choice, computer_Choice) {
     function draw(player_Choice, computer_Choice) {
     outcome_result_h4.innerHTML = player_Choice + " cancels out " + computer_Choice +", its a draw!.";
     }
-
-function game(player_Choice) {
-    let computer_Choice = computer_Selection();
-    switch(player_Choice + computer_Choice){
-        case 'rockscissors':
-        case 'rocklizard':
-            rock_win(player_Choice, computer_Choice);
-            break;
-        case 'paperrock':
-            paperrock_win(player_Choice, computer_Choice);
-            break;
-        case 'paperspock':
-            paperspock_win(player_Choice, computer_Choice);
-            break;
-        case 'scissorspaper':
-            scissorspaper_win(player_Choice, computer_Choice);
-            break;
-        case 'scissorslizard':
-            scissorslizard_win(player_Choice, computer_Choice);
-            break;
-        case 'lizardspock':
-            lizardspock_win(player_Choice, computer_Choice);
-            break;
-        case 'lizardpaper':
-            lizardpaper_win(player_Choice, computer_Choice);
-            break;
-        case 'spockrock':
-            spockrock_win(player_Choice, computer_Choice);
-            break;
-        case 'spockscissors':
-            spockscissors_win(player_Choice, computer_Choice);
-            break;
-        case 'scissorsrock':
-        case 'lizardrock':
-            rock_loss(player_Choice, computer_Choice);
-            break;
-        case 'rockpaper':
-            rockpaper_loss(player_Choice, computer_Choice);
-            break;
-        case 'spockpaper':
-            spockpaper_loss(player_Choice, computer_Choice);
-            break;
-        case 'paperscissors':
-            paperscissors_loss(player_Choice, computer_Choice);
-            break;
-        case 'lizardscissors':
-            lizardscissors_loss(player_Choice, computer_Choice);
-            break;
-        case 'spocklizard':
-            spocklizard_loss(player_Choice, computer_Choice);
-            break;
-        case 'paperlizard':
-            paperlizard_loss(player_Choice, computer_Choice);
-            break;
-        case 'rockspock':
-            rockspock_loss(player_Choice, computer_Choice);
-            break;
-        case 'scissorsspock':
-            scissorsspock_loss(player_Choice, computer_Choice);
-            break;
-        case 'rockrrock':
-        case 'sissorssissors':
-        case 'paperpaper':
-        case 'lizardlizard':
-        case 'spockspock':
-            draw(player_Choice, computer_Choice);
-            break;
-    }
-}
-
-function main(){
-    div_rock_selector.addEventListener('click', function() {
-        game("rock");
-    })
-    
-    div_paper_selector.addEventListener('click', function() {
-        game("paper");
-    })
-    
-    div_scissors_selector.addEventListener('click', function() {
-        game("scissors");
-    })
-    
-    div_lizard_selector.addEventListener('click', function() {
-        game("lizard");
-    })
-    
-    div_spock_selector.addEventListener('click', function() {
-        game("spock");
-    });
-}
-
-main();
